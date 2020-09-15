11bbcb1575dd2a5f46aeffbebbd14ae32d2bfae0
彭道宽 <pengdaokuan@cvte.com>
2020-09-15 12:04:21 +0800

    [feature][what]脚手架的搭建

---
+++ config/webpack.dev.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
+
+const devConfig = {
+  mode: 'development',
+  entry: path.join(__dirname, '../example/src/app.js'),
+  output: {
+    path: path.join(__dirname, '../dist/'),
+    filename: 'bundle.js', // 使用 webpack-dev-server 启动开发服务时，并不会实际在`src`目录下生成bundle.js，打包好的文件是在内存中的，但并不影响我们使用。
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /\.min\.css$/,
+        loader: ['style-loader', 'css-loader?modules'],
+      },
+      {
+        test: /\.min\.css$/,
+        loader: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, '../example/src/'),
+    compress: true,
+    host: '127.0.0.1', // webpack-dev-server启动时要指定ip，不能直接通过localhost启动，不指定会报错
+    port: 8001, // 启动端口为 3001 的服务
+    open: true, // 自动打开浏览器
+  },
+}
+
+module.exports = merge(devConfig, baseConfig) // 将baseConfig和devConfig合并为一个配置
---
+++ example/src/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import ReactDemo from '../../src' // 引入组件
+
+const App = () => <ReactDemo />
+render(<App />, document.getElementById('root'))
